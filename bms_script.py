57dddc148199da7852b5e0ed3ecbc7e95cd9476c
Gobel Power <34978431+fancyui@users.noreply.github.com>
2024-06-27 21:30:28 +0800

    Update bms_script.py

--- bms_script.py
+++ bms_script.py
@@ -89,3 +89,67 @@ def parse_all_pack_analog_ascii_response(response):
 response = "7E 32 35 30 30 34 36 30 30 46 30 37 00 02 03E8 03E8 03E8 02 1F4 1F4 07D0 07D0 07D0 03 1F4 1F4 07D0 07D0 07D0"
 data = parse_all_pack_analog_ascii_response(response)
 print(data)
+
+def send_data_to_bms(command):
+    """
+    Generates a request to send to the BMS to get specific data based on the command parameter.
+
+    Parameters:
+    command (str): The command to send to the BMS. It can be one of the following:
+                   - 'analog_data' for getting all pack analog information.
+                   - 'warning_data' for getting pack warning information.
+                   - 'capacity_data' for getting pack capacity information.
+    
+    Returns:
+    bytes: The request message to be sent to the BMS.
+    """
+    
+    SOI = '~'
+    VER = '32'
+    ADR = '35'
+    COMMON_PART = '30 30 34 36'
+    
+    if command == 'analog_data':
+        CID2 = '42'
+    elif command == 'warning_data':
+        CID2 = '44'
+    elif command == 'capacity_data':
+        CID2 = 'A6'
+    else:
+        raise ValueError("Invalid command")
+    
+    # Length of the INFO data in ASCII characters, set to 02H for all commands
+    LENID = '02'
+    
+    # Command for all packs, set to FFH for getting all pack information
+    COMMAND = 'FF'
+    
+    # Placeholder for length, checksum, and EOI to be calculated later
+    LENGTH_PLACEHOLDER = '00 00'
+    CHKSUM_PLACEHOLDER = '00'
+    EOI = '\r'
+    
+    # Construct the message without length and checksum
+    message_without_length_checksum = f"{SOI} {VER} {ADR} {COMMON_PART} {CID2} {LENID} {COMMAND}"
+    
+    # Calculate the length (excluding SOI and EOI)
+    length = len(message_without_length_checksum.split())
+    
+    # Calculate the checksum
+    checksum = 0
+    for char in message_without_length_checksum.replace(" ", ""):
+        checksum += ord(char)
+    checksum = (0x10000 - (checksum % 0x10000)) & 0xFFFF
+    
+    # Format checksum in hex
+    checksum_hex = f"{checksum:04X}"
+    
+    # Construct the final message
+    message = f"{message_without_length_checksum} {LENGTH_PLACEHOLDER} {checksum_hex} {EOI}"
+    
+    return message.encode('ascii')
+
+# Example usage
+request_message = send_data_to_bms('analog_data')
+print(request_message)
+
